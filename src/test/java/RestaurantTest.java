d0de0692dd1ea2e4639b3e56bb1afd5174698c8b
S-Ank04 <ankithas18@gmail.com>
2022-09-07 17:53:24 +0530

    Part 2: Solution

---
+++ src/test/java/RestaurantTest.java
@@ -0,0 +1,88 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.time.LocalTime;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@ExtendWith(MockitoExtension.class)
+class RestaurantTest {
+
+    Restaurant restaurant;
+
+    @BeforeEach
+    public void addDetails(){
+        LocalTime openingTime = LocalTime.parse("10:30:00");
+        LocalTime closingTime = LocalTime.parse("22:00:00");
+        restaurant =new Restaurant("Amelie's cafe","Chennai",openingTime,closingTime);
+        restaurant.addToMenu("Sweet corn soup",119);
+        restaurant.addToMenu("Vegetable lasagne", 269);
+    }
+
+    //>>>>>>>>>>>>>>>>>>>>>>>>>OPEN/CLOSED<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+    @Test
+    public void is_restaurant_open_should_return_true_if_time_is_between_opening_and_closing_time(){
+        LocalTime timeBetweenOpeningAndClosingTime = LocalTime.parse("13:00:00");
+        Restaurant restaurantSpy = Mockito.spy(restaurant);
+        Mockito.when(restaurantSpy.getCurrentTime()).thenReturn(timeBetweenOpeningAndClosingTime);
+
+        assertTrue(restaurantSpy.isRestaurantOpen());
+    }
+
+    @Test
+    public void is_restaurant_open_should_return_false_if_time_is_outside_opening_and_closing_time(){
+        //Arrange
+        LocalTime timeAfterClosingTime = LocalTime.parse("22:30:00");
+        Restaurant spiedRestaurant = Mockito.spy(restaurant);
+        Mockito.when(spiedRestaurant.getCurrentTime()).thenReturn(timeAfterClosingTime);
+
+        assertFalse(spiedRestaurant.isRestaurantOpen());
+
+    }
+
+    //<<<<<<<<<<<<<<<<<<<<<<<<<OPEN/CLOSED>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+
+
+    //>>>>>>>>>>>>>>>>>>>>>>>>>>>MENU<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+    @Test
+    public void adding_item_to_menu_should_increase_menu_size_by_1(){
+
+
+        int initialMenuSize = restaurant.getMenu().size();
+        restaurant.addToMenu("Sizzling brownie",319);
+        assertEquals(initialMenuSize+1,restaurant.getMenu().size());
+    }
+    @Test
+    public void removing_item_from_menu_should_decrease_menu_size_by_1() throws itemNotFoundException {
+
+
+        int initialMenuSize = restaurant.getMenu().size();
+        restaurant.removeFromMenu("Vegetable lasagne");
+        assertEquals(initialMenuSize-1,restaurant.getMenu().size());
+    }
+    @Test
+    public void removing_item_that_does_not_exist_should_throw_exception() {
+
+
+        assertThrows(itemNotFoundException.class,
+                ()->restaurant.removeFromMenu("French fries"));
+    }
+    //<<<<<<<<<<<<<<<<<<<<<<<MENU>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+
+
+    //Display method is called only once. Testing void method
+    @Test
+    public void display_method_is_called_once(){
+
+        Restaurant spiedRestaurant = Mockito.spy(restaurant);
+        Mockito.lenient().doNothing().when(spiedRestaurant).displayDetails();
+        spiedRestaurant.displayDetails();
+
+        Mockito.verify(spiedRestaurant,Mockito.times(1)).displayDetails();
+    }
+}
